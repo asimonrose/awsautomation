79d96122f84d2b5ca9642dfb98a962a951fbe021
rose.simon <rose.simon@ad.asdtrazo-prod.ice.internal>
2020-07-23 12:40:39 +0100

    resolved the etag issue

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python
-# -*- coding: utf -*-
+# -*- coding: utf-8 -*-
 """Webotron is the first app developed during the Automating AWS with Python course."""
 import boto3
 import click
-from pathlib import Path
+
 from bucket import BucketManager
+from domain import DomainManager
+import util
 
 session = None
 bucket_manager = None
+domain_manager = None
 
 
 @click.group()
 @click.option('--profile', default=None, help="Use a given AWS profile.")
 def cli(profile):
     """Webotron deploys websites to AWS."""
-    global session, bucket_manager
+    global session, bucket_manager, domain_manager
     session_cfg = {}
     if profile:
         session_cfg['profile_name'] = profile
 
     session = boto3.Session(**session_cfg)
     bucket_manager = BucketManager(session)
-    pass
+    domain_manager = DomainManager(session)
 
 
 @cli.command('list-buckets')
@@ -57,6 +60,20 @@ def sync(pathname, bucket):
     """Sync contents of PATHNAME to BUCKET."""
     bucket_manager.sync(pathname, bucket)
     print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
+
+
+@cli.command('setup-domain')
+@click.argument('domain')
+def setup_domain(domain):
+    """Configure DOMAIN to point to Bucket."""
+    bucket = bucket_manager.get_bucket(domain)
+
+    zone = domain_manager.find_hosted_zone(domain) \
+        or domain_manager.create_hosted_zone(domain)
+
+    endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
+    domain_manager.create_s3_domain_record(zone, domain, endpoint)
+    print("Domain configure: http://{}".format(domain))
 
 
 if __name__ == '__main__':
